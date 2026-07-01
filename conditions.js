982173bc1c009bba53004cba558d0a348807c5f4
Arshad ullah <jahan665577@gmail.com>
2026-07-01 10:39:10 +0500

    condition added

---
+++ conditions.js
@@ -0,0 +1,15 @@
+console.log("this is a test file for devops");
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+
+
+add(5, 3); // returns 8
+
+subtract(5, 3); // returns 2
