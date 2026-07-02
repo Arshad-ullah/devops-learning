f420da3e1860b3b13530741644121b1ddfc45545
Arshad ullah <jahan665577@gmail.com>
2026-07-02 09:31:57 +0500

    issue solve

--- devops.py
+++ devops.py
@@ -3,3 +3,9 @@ print("welcome to devops")
 # this is just testing file
 
 print("this is a test file for devops")
+
+
+a=32
+b=12
+print("Addition of 32 and 12 is: " + str(a+b))
+print("Subtraction of 32 and 12 is: " + str(a-b))
