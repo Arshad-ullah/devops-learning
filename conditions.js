ecc7e6723965cf754204cb854f3a2b7387fe4c82
Arshad ullah <jahan665577@gmail.com>
2026-07-02 09:21:53 +0500

    did some changes

--- conditions.js
+++ conditions.js
@@ -1,3 +1,4 @@
+// This is a test file for devops
 console.log("this is a test file for devops");
 
 function add(a, b) {
@@ -9,5 +10,32 @@ function subtract(a, b) {
 }
 
 
+console.log("--- Test Cases ---");
+
 console.log("Addition of 5 and 3 is: " + add(5, 3));
 console.log("Subtraction of 5 and 3 is: " + subtract(5, 3));
+
+
+// This is a test file for devops
+
+
+const obje = {
+    name: "John",
+    age: 30,
+    city: "New York",
+    address: {
+        street: "123 Main St",
+        zip: "10001"
+    }
+
+
+
+}
+
+
+
+console.log("Name: " + obje.name);
+console.log("Age: " + obje.age);
+console.log("City: " + obje.city);
+console.log("Street: " + obje.address.street);
+console.log("Zip: " + obje.address.zip);
