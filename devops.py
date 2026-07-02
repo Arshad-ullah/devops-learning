f2624f86ef8c861e7c30f574e1b83f97efc07baa
Arshad ullah <jahan665577@gmail.com>
2026-07-02 09:41:00 +0500

    for loop added

--- devops.py
+++ devops.py
@@ -9,3 +9,9 @@ a=32
 b=12
 print("Addition of 32 and 12 is: " + str(a+b))
 print("Subtraction of 32 and 12 is: " + str(a-b))
+
+
+for i in range(5):
+    print("This is iteration number: " + str(i))
+
+    
